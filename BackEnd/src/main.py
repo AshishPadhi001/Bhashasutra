09bcaef468b0b5bf6c9dae9cbae145553c200456
Ashish Padhi <padhiashish001@gmail.com>
2025-03-16 17:47:44 +0530

    Updated : Main.py

--- BackEnd/src/main.py
+++ BackEnd/src/main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
-from src.api.endpoints import basic,sentiment
+from src.api.endpoints import basic,sentiment,advanced,visualization
 
 
 app = FastAPI()
 
-# Include Routers
+# # Include Routers
 app.include_router(basic.router, prefix='/basic', tags=['Basic NLP'])
-# app.include_router(advanced.router, prefix='/advanced', tags=['Advanced NLP'])
+app.include_router(advanced.router, prefix='/advanced', tags=['Advanced NLP'])
 app.include_router(sentiment.router, prefix='/sentiment', tags=['Sentiment Analysis'])
-# app.include_router(visualization.router, prefix='/visualization', tags=['Text Visualization'])
+app.include_router(visualization.router, prefix='/visualization', tags=['Text Visualization'])
 # app.include_router(auth.router, prefix='/auth', tags=['Authentication'])
 # app.include_router(users.router, prefix='/users', tags=['Users'])
 # app.include_router(health.router, prefix='/health', tags=['Health'])
